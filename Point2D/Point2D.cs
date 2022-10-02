890937754dfcb25869dc5fa0ba2c2fa148d41adb
DMWebDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-10-02 20:13:13 +0700

    first commit

---
+++ Point2D/Point2D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contract;
+namespace Contract
+{
+    public class Point2D : IShape
+    {
+       
+            public string MagicWord { get => "Point"; }
+            public int X { get; set; }
+            public int Y { get; set; }
+        
+    }
+    public class PointToStringUIConverter : IShapeToStringUIConverter
+    {
+        public string MagicWord { get => "Point"; }
+        public string convert(IShape shape)
+        {
+            Point2D line = (Point2D)shape;
+            var builder = new StringBuilder();
+            builder.Append("(");
+            builder.Append(line.X);
+            builder.Append(",");
+            builder.Append(line.Y);
+            builder.Append(")");
+
+            string result = builder.ToString();
+            return result;
+        }
+
+        public IShape ConvertBack(string buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public class Rectangle : IShape
+    {
+        public string MagicWord { get => "Rectangle"; }
+        public Point2D TopLeft { get; set; }
+        public Point2D RightBottom { get; set; }
+    }
+    public class RectangleToStringDataConverter : IShapeToStringDataConverter
+    {
+        public string MagicWord { get => "Rectangle"; }
+
+        public string Convert(IShape shape)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Line ((2, 2), (1, 1))
+        public IShape ConvertBack(string buffer)
+        {
+            // Remove first word
+            int firstSpaceIndex = buffer.IndexOf(" ");
+            // 2, 2), (1, 1
+            string temp = buffer.Substring(firstSpaceIndex + 3,
+                buffer.Length - firstSpaceIndex - 5);
+
+            // 2, 2
+            // 1, 1
+            string[] parts = temp.Split(new string[] { "), (" }, StringSplitOptions.None);
+            string[] tokens1 = parts[0].Split(new string[] { ", " }, StringSplitOptions.None);
+            string[] tokens2 = parts[1].Split(new string[] { ", " }, StringSplitOptions.None);
+
+            var p1 = new Point2D() { X = int.Parse(tokens1[0]), Y = int.Parse(tokens1[1]) };
+            var p2 = new Point2D() { X = int.Parse(tokens2[0]), Y = int.Parse(tokens2[1]) };
+            IShape result = new Rectangle() { TopLeft = p1, RightBottom = p2 };
+            return result;
+        }
+    }
+    public class RectangleToStringUIConverter : IShapeToStringUIConverter
+    {
+        public string MagicWord { get => "Rectangle"; }
+
+        public string convert(IShape shape)
+        {
+            Rectangle rectangle = (Rectangle)shape;
+            var builder = new StringBuilder();
+
+            var pointConverter = new PointToStringUIConverter();
+            builder.Append(shape.MagicWord).Append(": ");
+            builder.Append("(").Append(pointConverter.convert(rectangle.TopLeft))
+                   .Append("), (").Append(pointConverter.convert(rectangle.RightBottom))
+                   .Append(")");
+
+            string result = builder.ToString();
+            return result;
+        }
+
+        public IShape ConvertBack(string buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
